c0ccfbe6a8d808e1e05132858d350a787969fcfc
Lior Mulay <lior@superup.me>
2020-01-08 13:19:51 +0200

    Create country model

---
+++ app/src/main/java/com/example/countries_information/Models/Country.java
@@ -0,0 +1,57 @@
+package com.example.countries_information.Models;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Country {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("borders")
+    @Expose
+    private List<String> borders = null;
+
+    @SerializedName("nativeName")
+    @Expose
+    private String nativeName;
+
+    @SerializedName("alpha3Code")
+    @Expose
+    private String alpha3Code;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getBorders() {
+        return borders;
+    }
+
+    public void setBorders(List<String> borders) {
+        this.borders = borders;
+    }
+
+    public String getNativeName() {
+        return nativeName;
+    }
+
+    public void setNativeName(String nativeName) {
+        this.nativeName = nativeName;
+    }
+
+    public String getAlpha3Code() {
+        return alpha3Code;
+    }
+
+    public void setAlpha3Code(String alpha3Code) {
+        this.alpha3Code = alpha3Code;
+    }
+}
