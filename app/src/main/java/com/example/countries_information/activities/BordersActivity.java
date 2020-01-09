8a6abec737619348836dacfae0c6a84c4f3631dd
Lior Mulay <lior@superup.me>
2020-01-09 20:08:07 +0200

    get Countries from intent

--- app/src/main/java/com/example/countries_information/activities/BordersActivity.java
+++ app/src/main/java/com/example/countries_information/activities/BordersActivity.java
@@ -1,18 +1,25 @@
 package com.example.countries_information.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import com.example.countries_information.R;
 import com.example.countries_information.adapters.CountriesAdapter;
-import com.example.countries_information.adapters.CountriesAdapterClickable;
+import com.example.countries_information.models.Country;
 
-public class BordersActivity extends AppCompatActivity {
+import java.util.List;
 
-    private RecyclerView countriesRecyclerView;
-    private CountriesAdapter countriesAdapter;
+import static com.example.countries_information.CountriesViewModel.BORDERS_EXTRA;
+import static com.example.countries_information.CountriesViewModel.COUNTRY_EXTRA;
+
+public class BordersActivity extends CountiesActivity {
+
+    private AppCompatTextView countyTextView;
+    private AppCompatTextView borderTextView;
+    private AppCompatTextView englishTextView;
+    private AppCompatTextView nativeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +27,41 @@ public class BordersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_borders);
         countriesAdapter = new CountriesAdapter(this);
         findViews();
+        initRecyclerCountries();
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            setOriginalCountry(extras);
+            setBorders(extras);
+        }
+    }
 
+    private void setBorders(Bundle extras) {
+        List<Country> countries = (List<Country>) extras.getSerializable(BORDERS_EXTRA);
+        if (countries != null && countries.size() > 0) {
+            borderTextView.setText(getString(R.string.borders));
+            countriesAdapter.setCountries(countries);
+            englishTextView.setVisibility(View.VISIBLE);
+            nativeTextView.setVisibility(View.VISIBLE);
+        } else {
+            borderTextView.setText(getString(R.string.borders_not_found));
+            englishTextView.setVisibility(View.GONE);
+            nativeTextView.setVisibility(View.GONE);
+        }
+    }
+
+    private void setOriginalCountry(Bundle extras) {
+        Country country = (Country) extras.getSerializable(COUNTRY_EXTRA);
+        if (country != null) {
+            countyTextView.setText(country.getName());
+        }
     }
 
     private void findViews() {
         countriesRecyclerView = findViewById(R.id.countries_recycler);
+        countyTextView = findViewById(R.id.county_original);
+        borderTextView = findViewById(R.id.borders_textView);
+        englishTextView = findViewById(R.id.english_textView);
+        nativeTextView = findViewById(R.id.native_textView);
     }
 
 
